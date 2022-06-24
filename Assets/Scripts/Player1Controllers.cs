2dd4aada9e2c3ed84505a7354137ad461c1330ff
cabbaacb <103894445+cabbaacb@users.noreply.github.com>
2022-06-24 23:02:30 +0300

    players
    added players and got player1 to be moved with buttons. no physical-ish things yet.

---
+++ Assets/Scripts/Player1Controllers.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controllers : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Rigidbody _rigidBody;
+
+    private Player1Actions _playerActions;
+    private void Awake()
+    {
+
+        _rigidBody = GetComponent<Rigidbody>();
+        _playerActions = new Player1Actions();
+    }
+
+    private void FixedUpdate()
+    {
+        Moving();
+    }
+
+    private void OnEnable()
+    {
+        _playerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerActions.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        _playerActions.Dispose();
+    }
+
+    private void Moving()
+    {
+        float horizontalInput = _playerActions.Player.MovingHorizontal.ReadValue<float>();
+        float verticalInput = _playerActions.Player.MovingVertical.ReadValue<float>();
+
+        Vector3 movingHorizontal = transform.forward * _speed * horizontalInput * Time.fixedDeltaTime;
+        Vector3 movingVertical = transform.right * _speed * verticalInput * Time.fixedDeltaTime;
+
+        _rigidBody.MovePosition(_rigidBody.position + movingHorizontal + movingVertical);
+    }
+}
---
+++ Assets/Scripts/Player2Controllers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controllers : MonoBehaviour
+{
+    private Player2Actions _playerActions;
+    private void Awake()
+    {
+        _playerActions = new Player2Actions();
+    }
+
+    private void OnEnable()
+    {
+        _playerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerActions.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        _playerActions.Dispose();
+    }
+}
