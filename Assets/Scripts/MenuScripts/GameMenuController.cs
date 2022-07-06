a62c62bc95df54f5d0de901ddd82f4b5d99ab73b
cabbaacb <103894445+cabbaacb@users.noreply.github.com>
2022-07-06 17:05:01 +0300

    menu Scene
    created menu scene, added 2 buttons

---
+++ Assets/Scripts/MenuScripts/GameMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameMenuPanel;
+    [SerializeField] private Button _newGameButton;
+    [SerializeField] private Button _exitButton;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _newGameButton.onClick.AddListener(StartNewGame);
+        _exitButton.onClick.AddListener(ExitGame);
+    }
+
+    private void OnDestroy()
+    {
+        _newGameButton.onClick.RemoveAllListeners();
+        _exitButton.onClick.RemoveAllListeners();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartNewGame()
+    {
+
+    }
+
+    private void ExitGame()
+    {
+        print("Exit");
+        Application.Quit();
+        //EditorApplication.isPlaying = false;
+    }
+}
